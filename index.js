d4982001fdde4297bed4a766b03b1d6105520d64
michaelleighstewart <michael.leigh.stewart@gmail.com>
2020-02-18 21:11:09 -0800

    Saving changes

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const mysqlx = require('@mysql/xdevapi');
+
+const config = {
+    password: '8Isc4IaP6vuIpAw3LzfU',
+    user: 'admin',
+    host: 'tipping-db.cpghamdpwimb.us-west-2.rds.amazonaws.com',
+    port: 3306,
+    schema: 'tipping'
+};
+
+mysqlx
+    .getSession(config)
+    .then(session => {
+        console.log(session.inspect());
+        console.log('connected to mysql server!');
+    });
+
+
 client.on('ready', () => {
     console.log('logged in as ${client.user.tag}');
 });
 
 client.on('message', msg => {
     if (msg.content === 'ping') {
-        msg.reply('pong');
+        msg.reply('pong :Smug:');
     }
 });
 
